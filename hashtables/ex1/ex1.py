28b14f3452adca11b317c48b567c51ae98aaaa17
Francis Chen <fncis.chen@gmail.com>
2019-08-09 08:59:35 -0700

    finish hash example 1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -8,13 +8,40 @@ from hashtables import (HashTable,
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
+    
+    i = 0
+    for weight in weights:
+        hash_table_insert(ht, weight,i)
+        i += 1 
 
-    """
-    YOUR CODE HERE
-    """
+    # i.e. {4: 0}, {6: 1}, {10: 2}, {15: 3}, {16: 4}
+    # first step:
+    # loop through each weight in weights
 
+    for first_weight in weights:
+
+        # second step
+        # subtract 21 from that key
+        first_index = hash_table_retrieve(ht, first_weight)
+        second_weight = limit - first_weight
+
+        print(first_index,second_weight)
+        # third step
+        # search for that second value on the hash table
+
+        if hash_table_retrieve(ht, second_weight) != None:
+            second_index = hash_table_retrieve(ht, second_weight)
+            
+            merged_packages = [first_index, second_index]
+            merged_packages.sort(reverse=True)
+            return merged_packages
+    
+        # if it doesnt exist, skip to next item in hash table 
     return None
 
+
+e = [ 4, 6, 10, 15, 16 ]
+print(get_indices_of_item_weights(e,5,21))
 
 def print_answer(answer):
     if answer is not None:
